d2c13661113a2d0aeb9b3702b878ae5ff013dbd1
github username <github email address>
2018-01-29 18:26:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(array, name){
+  let new_array = [];
+  for (const i of array){
+    if (i.toLowerCase() === name.toLowerCase()) {
+      new_array.push(i);
+    }
+  }
+  return new_array;
+}
+
+function fuzzyMatch(array, name){
+  let new_array = [];
+  for (const i of array){
+    if ((i[0].toLowerCase() === name[0].toLowerCase()) && (i[1].toLowerCase() === name[1].toLowerCase())) {
+      new_array.push(i);
+    }
+  }
+  return new_array;
+}
+
+function matchName(array,name){
+  let new_array = [];
+  for (const i of array){
+    if (i.name === name) {
+      new_array.push(i);
+    }
+  }
+  return new_array;
+}
